4f2507bae65393de9b4ae6f471a474c0a83c02b0
Ghazale <ghsh88@gmail.com>
2015-04-27 19:35:30 +0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 		<div class="site-info">
             <?php printf( __( 'Copyright &copy; %s %s. All Rights Reserved.', 'sh-ghazale' ), date( 'Y' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?>
 			<span class="sep"> | </span>
-			<?php printf( __( '<i class="fa fa-cloud"></i> Designed By: %1$s', 'sh_ghazale' ), 'Ghazale'); ?>
+			<?php printf( __( '<i class="fa fa-cloud"></i> Designed By: %1$s', 'sh_ghazale' ), '<a href ="https://github.com/ghsh88">Ghazale</a>'); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
